215559ad6c028aea90fbe2eff36a73ad69b4b4e0
Gaurav Kumar <kumar.gauravnitk@gmail.com>
2016-09-12 14:06:25 +0200

    Hello Spark Scala

---
+++ spark_test/src/main/Scala/com/learning/spark/WordCount.scala
@@ -0,0 +1,24 @@
+package com.learning.spark
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object WordCount {
+  def main(args: Array[String]) = { 
+  
+  val conf = new SparkConf().setAppName("WordCount").setMaster("local")
+  
+  val sc = new SparkContext(conf)
+  
+  val inputpath = args(0)
+  val outputpath = args(1)
+  
+  val wc = sc.textFile(inputpath).flatMap(x => x.split(" ")).map( x =>(x,1)).reduceByKey((acc,value) => acc+ value)
+  
+  wc.saveAsTextFile(outputpath)
+  println("#######################################################################")
+  println("Word Count COMPLETED SUCCESSFULLY")
+  println("#######################################################################")
+  
+  }
+}
